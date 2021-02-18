26cfd3bb00a21424b92377326ac35aa7a46c983c
Rahul Bhatia <rahbha@on.sinch.com>
2021-02-18 18:44:32 +0530

    Adding demo3

---
+++ terraform-course-master/demo-3/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+    default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-01aab85a5e4a5a0fe"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
